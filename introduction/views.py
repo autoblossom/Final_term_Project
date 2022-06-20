57518ac190079bfca6c0c01ac2ab1f3459d86e79
ParkJinSu <wlstnwkd3411@naver.com>
2022-06-20 15:17:22 +0900

    seventh commit

--- introduction/views.py
+++ introduction/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
 from django.template import loader
+from .models import Question
+
+
 
 
 def index(request):
@@ -9,3 +12,10 @@ def index(request):
     context ={
     }
     return HttpResponse(template.render(context,request))
+
+def detail(req,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("question does not exist")
+    return render(req,'polls/detail.html',{'question':question})
