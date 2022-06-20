5adc2643468eb90c19f729c4fe3b42bba7873ec7
ParkJinSu <wlstnwkd3411@naver.com>
2022-06-20 15:04:25 +0900

    fifth commit

--- introduction/views.py
+++ introduction/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from django.http import Http404
 
-from .models import Question
 
 def index(request):
     
     return render(request, "introduction/index.html")
-
-class Index(View):
-    def get(request):
-        return render(request, "main/index.html")
